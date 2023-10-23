c6d28b495c378c0fd06c8d9a145934f8c1829ff1
Gustavo Biaso <gustavobiaso@gmail.com>
2023-10-23 17:03:44 -0300

    Setup para Tags

---
+++ src/main/java/net/guinimos/skilltreemod/util/ModTags.java
@@ -0,0 +1,24 @@
+package net.guinimos.skilltreemod.util;
+
+import net.guinimos.skilltreemod.SkillTreeMod;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+    public static class Blocks {
+        private static TagKey<Block> tag(String name){
+            return BlockTags.create(new ResourceLocation(SkillTreeMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+
+        private static TagKey<Item> tag(String name){
+            return ItemTags.create(new ResourceLocation(SkillTreeMod.MOD_ID, name));
+        }
+    }
+}
